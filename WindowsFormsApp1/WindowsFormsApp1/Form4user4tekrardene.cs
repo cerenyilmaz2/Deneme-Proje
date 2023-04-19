04177dd9d55e48be0d63489d5c3e5a49314d0230
cerenyilmaz2 <cerenyilmazzz@gmal.com>
2023-04-19 22:19:11 +0300

    form1değişiklik silindi

--- WindowsFormsApp1/WindowsFormsApp1/Form4user4tekrardene.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form4user4tekrardene.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Form4user4tekrardene : Form
     {
-        public Form1()
+        public Form4user4tekrardene()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            label1.Text = "Düzenlendi";
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "tekrar denedi";
         }
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Formuser4.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Formuser4.cs
@@ -10,21 +10,15 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Formuser4 : Form
     {
-        public Form1()
+        public Formuser4()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Düzenlendi";
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "tekrar denedi";
-        }
+            label1.Text = "yeni form ";        }
     }
 }
